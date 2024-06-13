e101a6c38262554546b3a58a38c3ec5101984ba4
BrianZ93 <brianzanayed@gmail.com>
2024-06-13 17:51:52 -0500

    formatting

--- web_engine/bachelor_party/public_html/assets/js/creative-studio.js
+++ web_engine/bachelor_party/public_html/assets/js/creative-studio.js
@@ -10,23 +10,36 @@
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/ 
+*/
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+  $(".navbar .nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-        if (this.hash !== "") {
+      var hash = this.hash;
 
-            event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
 
-            var hash = this.hash;
+function selectTab(tab) {
+  document.getElementById("attending-tab").classList.remove("active");
+  document.getElementById("regrets-tab").classList.remove("active");
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
-}); 
+  if (tab === "attending") {
+    document.getElementById("attending-tab").classList.add("active");
+  } else {
+    document.getElementById("regrets-tab").classList.add("active");
+  }
+}
